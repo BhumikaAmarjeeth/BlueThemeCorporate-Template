5513b7e5d187cf858ce99f8f6769ad11d661a2ae
fulgid <navin@fulgid.in>
2020-05-28 10:33:46 +0530

    menu hover fixed, testimonial section and some scroll animation fixed

--- js/script.js
+++ js/script.js
@@ -200,3 +200,26 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
 	event.preventDefault();
 	$(this).ekkoLightbox();
 });
+
+
+// Bind to scroll
+$(window).scroll(function(){
+	// Get container scroll position
+	var fromTop = $(this).scrollTop()+topMenuHeight;
+	// Get id of current scroll item
+	var cur = scrollItems.map(function(){
+		if ($(this).offset().top < fromTop)
+			return this;
+	});
+	// Get the id of the current element
+	cur = cur[cur.length-1];
+	var id = cur && cur.length ? cur[0].id : "";
+
+	if (lastId !== id) {
+		lastId = id;
+		// Set/remove active class
+		menuItems
+			.parent().removeClass("active1")
+			.end().filter("[href='#"+id+"']").parent().addClass("active1");
+	}
+});
